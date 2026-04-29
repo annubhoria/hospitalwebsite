699793d3cdc6a809cd3633a6ac6d5ca818782d42
sanjeev singh <sanjeevsinghdesk@gmail.com>
2026-04-30 01:42:44 +0530

    full code added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App(){
+  return (
+    <div style={{fontFamily:'sans-serif',padding:'20px'}}>
+      <h1>Blessings Hospital Sonipat</h1>
+      <p>Mother & Child Care Hospital</p>
+
+      <a href="tel:+919518691607">Call Now</a><br/>
+      <a href="https://wa.me/919518691607" target="_blank">WhatsApp</a><br/>
+
+      <iframe
+        src="https://maps.google.com/maps?q=Blessings%20Hospital%20Sonipat&output=embed"
+        width="100%"
+        height="300"
+      />
+    </div>
+  )
+}
